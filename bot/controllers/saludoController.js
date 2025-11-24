e7d9971451edaac08658d979c5ce5c08f9742db6
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-24 12:12:18 -0500

    Update saludoController.js

--- bot/controllers/saludoController.js
+++ bot/controllers/saludoController.js
@@ -1,1 +1,11 @@
+// /bot/controllers/saludoController.js
+import { MENSAJES } from "../utils/messages.js";
+
+const saludoController = {
+  saludar() {
+    return MENSAJES.saludo_inicial;
+  }
+};
+
+export default saludoController;
 
