dc00dc39f0236ade5ae70a38e3dd1cb5bb2846d5
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-24 12:12:37 -0500

    Update visitasController.js

--- bot/controllers/visitasController.js
+++ bot/controllers/visitasController.js
@@ -1,1 +1,14 @@
+// /bot/controllers/visitasController.js
+
+const visitasController = {
+  async agendar() {
+    return (
+      "Perfecto, puedo ayudarte a coordinar una visita 👀🏡.\n" +
+      "Indícame por favor el día y hora aproximada en la que estarías disponible " +
+      "y un asesor se pondrá en contacto contigo para confirmarlo. 🤝"
+    );
+  }
+};
+
+export default visitasController;
 
