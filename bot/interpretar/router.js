c5a3c04c6a2477b8bc2341b200ca92682e8b106a
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-24 12:11:01 -0500

    Update router.js

--- bot/interpretar/router.js
+++ bot/interpretar/router.js
@@ -1,1 +1,27 @@
+// /bot/interpretar/router.js
+// -------------------------------------------------------
+// Direcciona la intención hacia el controlador correspondiente.
+// -------------------------------------------------------
+
+import propiedadesController from "../controllers/propiedadesController.js";
+import saludoController from "../controllers/saludoController.js";
+import ayudaController from "../controllers/ayudaController.js";
+import { MENSAJES } from "../utils/messages.js";
+
+export async function routeIntent(intencion, filtros, contexto = {}) {
+  switch (intencion) {
+    case "buscar_propiedades":
+      return await propiedadesController.buscar(filtros, contexto);
+
+    case "saludo":
+    case "saludo_simple":
+      return saludoController.saludar();
+
+    case "despedida":
+      return MENSAJES.despedida;
+
+    default:
+      return ayudaController.generica(contexto);
+  }
+}
 
