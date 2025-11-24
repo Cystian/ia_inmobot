0e0bfd2ac34b98ad15bda42c11f3741c3cbe38e3
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-24 12:14:26 -0500

    Update log.js

--- bot/utils/log.js
+++ bot/utils/log.js
@@ -1,1 +1,10 @@
+// /bot/utils/log.js
+
+export function logInfo(label, data) {
+  console.log(`ℹ️ [${label}]`, data);
+}
+
+export function logError(label, error) {
+  console.error(`❌ [${label}]`, error);
+}
 
