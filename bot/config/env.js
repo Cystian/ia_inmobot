43c15e2d6239d39aab43bece68816ceb84c7f4d6
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-24 12:14:45 -0500

    Update env.js

--- bot/config/env.js
+++ bot/config/env.js
@@ -1,1 +1,11 @@
+// /bot/config/env.js
+
+export const GROQ_API_KEY = process.env.GROQ_API_KEY;
+
+export const META_VERIFY_TOKEN = process.env.META_VERIFY_TOKEN;
+export const META_ACCESS_TOKEN = process.env.META_ACCESS_TOKEN;
+export const META_PHONE_NUMBER_ID = process.env.META_PHONE_NUMBER_ID;
+
+export const FRONTEND_BASE_URL =
+  process.env.FRONTEND_BASE_URL || "https://tuweb.com";
 
