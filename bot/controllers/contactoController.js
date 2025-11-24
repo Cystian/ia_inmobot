5c2ee475aad731f4613a62329a7ec39a3a903ed0
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-24 12:12:47 -0500

    Update contactoController.js

--- bot/controllers/contactoController.js
+++ bot/controllers/contactoController.js
@@ -1,1 +1,14 @@
+// /bot/controllers/contactoController.js
+
+const contactoController = {
+  solicitarDatos() {
+    return (
+      "Para acompañarte mejor, ¿podrías indicarme tu nombre y un horario " +
+      "en el que sueles estar disponible? 😊\n" +
+      "Así un asesor te contacta de manera personalizada."
+    );
+  }
+};
+
+export default contactoController;
 
